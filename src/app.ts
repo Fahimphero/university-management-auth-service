f643093bd197ec77d11d1c6765db13f3293cdd67
Fahimphero <irfanulislamfahim@gmail.com>
2023-07-06 05:12:45 +0600

    [DONE] Error handling full setup

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import cors from 'cors'
-import express, { Application, Request, Response } from 'express'
-import usersRouter from './app/modules/users/users.route'
+import express, { Application } from 'express'
+import globalErrorHandler from './app/middlewares/globalErrorHandler'
+import { UserRoutes } from './app/modules/users/user.route'
+
 const app: Application = express()
 
 app.use(cors())
@@ -10,12 +12,18 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // Application Routes
-app.use('/api/v1/users/', usersRouter)
+app.use('/api/v1/users/', UserRoutes)
 // console.log(process.env)
 
 // Testing
-app.get('/', async (req: Request, res: Response) => {
-  res.send('Working Successfully')
-})
+// app.get('/', async (req: Request, res: Response, next: NextFunction) => {
+//   //   console.log(x)
+//   //   Promise.reject(new Error('Unhandled Promise Rejection'))
+//   throw new Error('Testing Error logger')
+//   // next('Ore baba Error')
+// })
+
+// Global Error Handler
+app.use(globalErrorHandler)
 
 export default app
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,49 @@
+/* eslint-disable no-console */
+import { Server } from 'http'
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config/index'
 import { errorlogger, logger } from './shared/logger'
 
+process.on('uncaughtException', error => {
+  errorlogger.error(error)
+  process.exit(1)
+})
+
+let server: Server
+
 async function bootstrap() {
   try {
     await mongoose.connect(config.database_url as string)
     logger.info('😁Database is connected successfully')
-    app.listen(config.port, () => {
+
+    server = app.listen(config.port, () => {
       logger.info(`Application listening on port ${config.port}`)
     })
   } catch (err) {
     errorlogger.error('Failed to connect database', err)
   }
 
+  process.on('unhandledRejection', error => {
+    console.log('Unhandled rejection detected, we are closing our server....')
+    if (server) {
+      server.close(() => {
+        errorlogger.error(error)
+        process.exit(1)
+      })
+    } else {
+      process.exit(1)
+    }
+  })
+
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
 bootstrap()
+
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM is recieved')
+  if (server) {
+    server.close()
+  }
+})
