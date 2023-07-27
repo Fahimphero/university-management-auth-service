c3597e66f712499188177967596977d52b0b7051
abedinforhan <abedinforhan@gmail.com>
2023-07-27 12:09:25 +0600

    latest

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,52 +1,22 @@
 import express from 'express';
-import { AcademicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.routes';
-import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.route';
-import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.route';
-import { AdminRoutes } from '../modules/admin/admin.route';
-import { AuthRoutes } from '../modules/auth/auth.route';
-import { FacultyRoutes } from '../modules/faculty/faculty.route';
-import { ManagementDepartmentRoutes } from '../modules/managementDepartment/managementDepartment.route';
-import { StudentRoutes } from '../modules/student/student.route';
-import { UserRoutes } from '../modules/user/user.route';
+import { BrandRoutes } from '../modules/brand/brand.route';
+import { CategoryRoutes } from '../modules/category/category.route';
+import { ProductRoutes } from '../modules/product/product.route';
 
 const router = express.Router();
 
 const moduleRoutes = [
   {
-    path: '/academic-semesters',
-    route: AcademicSemesterRoutes,
+    path: '/brands',
+    route: BrandRoutes,
   },
   {
-    path: '/academic-faculties',
-    route: AcademicFacultyRoutes,
+    path: '/categories',
+    route: CategoryRoutes,
   },
   {
-    path: '/academic-departments',
-    route: AcademicDepartmentRoutes,
-  },
-  {
-    path: '/management-departments',
-    route: ManagementDepartmentRoutes,
-  },
-  {
-    path: '/students',
-    route: StudentRoutes,
-  },
-  {
-    path: '/faculties',
-    route: FacultyRoutes,
-  },
-  {
-    path: '/admins',
-    route: AdminRoutes,
-  },
-  {
-    path: '/users',
-    route: UserRoutes,
-  },
-  {
-    path: '/auth',
-    route: AuthRoutes,
+    path: '/products',
+    route: ProductRoutes,
   },
 ];
 
